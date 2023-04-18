45a31772764b01aa64eb6495067871fe3cd549f8
jeon soomin <soomis7@naver.com>
2023-04-18 10:50:53 +0900

    Time Slider, Rewind, Fast forward, Volume Controls

--- Podcast/View/PlayerDetailsView.swift
+++ Podcast/View/PlayerDetailsView.swift
@@ -45,11 +45,47 @@ class PlayerDetailsView:UIView{
     }
     
     //MARK: -IBOutlet
-    //재생 정지 버튼
+    
+    
     @IBOutlet weak var currentTimeSlider: UISlider!
     @IBOutlet weak var currentTimeLabel: UILabel!
     @IBOutlet weak var durationTimeLabel: UILabel!
     
+    @IBAction func handleFastForward(_ sender: Any) {
+        seekToCurrentTime(delta: 15)
+    }
+    
+    @IBAction func handleRewind(_ sender: Any) {
+        
+        seekToCurrentTime(delta: -15)
+    }
+    
+    fileprivate func seekToCurrentTime(delta: Int64){
+        
+        let fifteenSeconds = CMTimeMake(value: delta, timescale: 1)
+        let seekTime = CMTimeAdd(player.currentTime(), fifteenSeconds)
+
+        player.seek(to: seekTime)
+    }
+    
+    @IBAction func handleVolumeChange(_ sender: UISlider) {
+        player.volume = sender.value
+    }
+    
+    @IBAction func handleCurrentTimeSliderChange(_ sender: Any) {
+        let percentage = currentTimeSlider.value
+        
+        guard let duration = player.currentItem?.duration else { return }
+        
+        let durationInSeconds = CMTimeGetSeconds(duration)
+        let seekTimeInSeconds = Float64(percentage) * durationInSeconds
+        
+        let seekTime = CMTimeMakeWithSeconds(seekTimeInSeconds, preferredTimescale: 1)
+        
+        player.seek(to: seekTime)
+    }
+    
+    //재생 정지 버튼
     @IBOutlet weak var playPauseButton: UIButton! {
         didSet {
             playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
